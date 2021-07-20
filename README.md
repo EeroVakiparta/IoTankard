df6d2edcdbf2a68fb61f6d636cc25aaad4598df4
EeroVakiparta <eero.vakiparta@student.samk.fi>
2021-07-20 21:26:52 +0300

    Planned pins and did the finger buttons

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # IoTankard
+
+Curious IoT test project.
+
+
+# Pin allocation
+
+| Device        | Pins              | Notes                  |
+| ------------- |:-------------:    | ----------------------:|
+| MX Buttons    | D4,D5,D6,D7       |  Add 10k resistors     |
+| HC-05 BT      | D2,D3             |   RX, TX               |
+| OLED I2D      | A4,A5             |   SCL, SDA             |
+| ADXL345       | D10,D11,D12,D13   |        SPI             |
+| MotorDriver   | D9 (PWM)          |    Only one needed     |
+| IF            | A6                |                        |
+
+3 free digital and 5 analog pins
---
+++ iotankard.ino
@@ -0,0 +1,60 @@
+int buttonDelay = 100;
+int buttonPressed = 0;
+
+const int indexFinger_ButtonPin = 5;
+const int middleFinger_ButtonPin = 6;
+const int ringFinger_ButtonPin = 7;
+const int pinkey_ButtonPin = 8;
+
+int indexFinger_Btn;
+int middleFinger_Btn;
+int ringFinger_Btn;
+int pinkey_Btn;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(indexFinger_ButtonPin, INPUT);
+  pinMode(middleFinger_ButtonPin, INPUT);
+  pinMode(ringFinger_ButtonPin, INPUT);
+  pinMode(pinkey_ButtonPin, INPUT);
+  Serial.println("SETUP DONE");
+  delay(10);
+}
+
+int readButton(int pin) {
+  pin = digitalRead(pin);
+  if (pin == 1) {
+    delay(50);
+    pin = digitalRead(pin);
+    if (pin == 0) {
+      return HIGH;
+    }
+  }
+}
+
+void loop() {
+  buttonPressed = 0;
+
+  indexFinger_Btn = readButton(indexFinger_ButtonPin);
+  middleFinger_Btn = readButton(middleFinger_ButtonPin);
+  ringFinger_Btn = readButton(ringFinger_ButtonPin);
+  pinkey_Btn = readButton(pinkey_ButtonPin);
+
+  if (indexFinger_Btn == HIGH && buttonPressed == 0 ) {
+    Serial.println("Q pressed ");
+    delay(buttonDelay);
+    buttonPressed = 1;
+  } else if (middleFinger_Btn == HIGH && buttonPressed == 0) {
+    Serial.println("W pressed ");
+    delay(buttonDelay);
+    buttonPressed = 1;
+  } else if (ringFinger_Btn == HIGH && buttonPressed == 0) {
+    Serial.println("E pressed ");
+    delay(buttonDelay);
+    buttonPressed = 1;
+  } else if (pinkey_Btn == HIGH && buttonPressed == 0) {
+    Serial.println("R pressed ");
+    delay(buttonDelay);
+    buttonPressed = 1;
+  }
+}
