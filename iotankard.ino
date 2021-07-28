9154dcdbc990d78c5109caf369869d705c6944ce
EeroVakiparta <eero.vakiparta@student.samk.fi>
2021-07-28 15:05:33 +0300

    Base startup screen

--- iotankard.ino
+++ iotankard.ino
@@ -1,6 +1,19 @@
 #include <SparkFun_ADXL345.h> // accelerometer
 #include <SoftwareSerial.h>  // Bluetooth
 
+// Screen //
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 32 
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 // Bluetooth //
 SoftwareSerial bt(2,3); // RX/TS pins as parameters remember to cross wire these
 
@@ -30,7 +43,20 @@ int pinkey_Btn;
 void setup() {
   Serial.begin(9600);
   bt.begin(9600);
-  /* Accelerometer */
+
+  /* Screen */
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Clear the buffer
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  delay(200);
+  
+  
   adxl.powerOn();
   adxl.setRangeSetting(16);           // range in G (2,4,8,16) Drawback with higher Gs is lower sensitivity
   adxl.setSpiBit(0);                  // 0 = 4wire SPI , 1=3wire SPI
@@ -70,6 +96,7 @@ void setup() {
 
   Serial.println("SETUP DONE");
   bt.println("BT SETUP DONE");
+  drawtext("IoTankard");
   delay(10);
 }
 
@@ -120,6 +147,18 @@ void loop() {
     delay(buttonDelay);
     buttonPressed = 1;
   }
+}
+
+// --- Screen functions --- //
+void drawtext(String texti) {
+  display.clearDisplay();
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  //display.println(F("test"));
+  display.println(texti);
+  display.display();      // Show initial text
+  delay(100);
 }
 
 // --- Button functions --- //
